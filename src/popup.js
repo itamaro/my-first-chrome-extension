50c8de3480330edee243f040984f86144e9f4f32
Itamar Ostricher <itamarost@gmail.com>
2021-04-21 13:37:43 -0700

    Add button to reset background color

--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,6 @@
 // Initialize button with user's preferred color
 let changeColor = document.getElementById("changeColor");
+let resetColor = document.getElementById("resetColor");
 
 chrome.storage.sync.get("color", ({ color }) => {
   changeColor.style.backgroundColor = color;
@@ -21,4 +22,19 @@ function setPageBackgroundColor() {
   chrome.storage.sync.get("color", ({ color }) => {
     document.body.style.backgroundColor = color;
   });
+}
+
+resetColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: clearPageBackgroundColor,
+  });
+});
+
+// The body of this function will be executed as a content script inside the
+// current page
+function clearPageBackgroundColor() {
+  document.body.style.backgroundColor = null;
 }
